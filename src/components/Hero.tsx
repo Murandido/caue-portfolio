efbb06d122d2b8e311d2341b7e6ec698ba998f3d
Murandido <murilo.candidomc97@gmail.com>
2023-06-28 15:32:44 -0300

    Responsive Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
-    <div className="flex h-[45rem] items-center">
-      <div className="ml-40 w-[80rem] text-[4rem]">
+    <div className="flex min-w-[338.55px]">
+      <div className="mx-40 my-60 w-[80rem] text-[4rem] max-lg:mx-10 max-sm:text-xl max-xs:mx-5">
         <span className="text-primary-300">Your brand is unique</span> and
         deserves a visual identity that represents it in an authentic and{" "}
         <span className="text-primary-300">unforgettable</span> way.
