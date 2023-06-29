244fbc42c15e0c18a4f3da7bf8162c9f9d022a39
Murandido <murilo.candidomc97@gmail.com>
2023-06-29 14:12:37 -0300

    footer done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import "../styles/ProjectLink.css";
 import { Roboto_Slab as Roboto, Righteous } from "next/font/google";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const robotoSlab = Roboto({
   subsets: ["latin"],
@@ -19,10 +20,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body
-        className={`${robotoSlab.variable} ${righteous.variable} scroll-smooth bg-secondary-300 font-righteous selection:bg-primary-300 selection:text-secondary-200`}
+        className={`${robotoSlab.variable} ${righteous.variable} scroll-smooth bg-secondary-300 font-righteous text-black-300 selection:bg-primary-300 selection:text-secondary-200`}
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import instagramLogoSVG from "@/assets/instagramLogo.svg";
+import behanceLogoSVG from "@/assets/behanceLogo.svg";
+
+export default function Footer() {
+  return (
+    <footer className="flex min-w-[338.55px] justify-center">
+      <div className="flex flex-col justify-center gap-6 p-16 max-sm:gap-3 max-sm:p-8">
+        <div className="text-[2.5rem] max-sm:text-lg">
+          Have a project?{" "}
+          <a className="text-primary-300 hover:underline" href="#">
+            contact
+          </a>
+        </div>
+        <div className="flex items-center justify-center text-base">
+          <div className="h-[1px] w-full bg-black-300"></div>
+          <div className="absolute bg-secondary-300 p-1 max-sm:p-[0.3rem] max-sm:text-[0.5rem] max-sm:leading-3">
+            and follow us
+          </div>
+        </div>
+        <div className="flex justify-center gap-4">
+          <a href="https://www.instagram.com/studiocauehenrique/">
+            <Image
+              className="w-12 transition-transform hover:scale-105 max-sm:w-5"
+              src={instagramLogoSVG}
+              alt="Instagram logo"
+              width={48}
+              height={48}
+            />
+          </a>
+          <a href="https://www.behance.net/cauehenryque">
+            <Image
+              className="w-12 transition-transform hover:scale-105 max-sm:w-5"
+              src={behanceLogoSVG}
+              alt="Behance logo"
+              width={48}
+              height={48}
+            />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
