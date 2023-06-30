7c9d3cc670ab2de26f0dd7c816a467b53120ab12
Murandido <murilo.candidomc97@gmail.com>
2023-06-29 22:45:20 -0300

    fixed images sizes

--- src/app/works/reallimpeza/page.tsx
+++ src/app/works/reallimpeza/page.tsx
@@ -23,18 +23,21 @@ export default function RealLimpezaPage() {
       <div className="min-w-[338.55px]">
         <div className="m-5 flex flex-wrap items-center justify-center gap-5">
           <Image
+            className="w-[116rem]"
             src="https://drive.google.com/uc?export=view&id=1a9l6kGZv3MfjMmqo0DtRIPMO9t6J9kG5"
             alt="Real Limpeza 1"
             width="1880"
             height="966"
           />
           <Image
+            className="w-[116rem]"
             src="https://drive.google.com/uc?export=view&id=17npfGp9IFXR6kBvPSVyyYOyRCpA5M1Wm"
             alt="Real Limpeza 2"
             width="1880"
             height="1224"
           />
           <Image
+            className="w-[116rem]"
             src="https://drive.google.com/uc?export=view&id=1qe7xK83QfT5YhW3hPn0R8dNHAjmPUi-U"
             alt="Real Limpeza 3"
             width="1880"
@@ -42,12 +45,14 @@ export default function RealLimpezaPage() {
           />
           <div className="flex flex-wrap items-center justify-center gap-5">
             <Image
+              className="w-[57.5rem]"
               src="https://drive.google.com/uc?export=view&id=1t5Mi5DMHaigeuFZuYxMFn83PFeufNyMC"
               alt="Real Limpeza 4"
               width="930"
               height="800"
             />
             <Image
+              className="w-[57.5rem]"
               src="https://drive.google.com/uc?export=view&id=130C30PaaPxX7YI4NQhhBJs091VH1N-TK"
               alt="Real Limpeza 5"
               width="930"
@@ -56,18 +61,21 @@ export default function RealLimpezaPage() {
           </div>
           <div className="flex flex-wrap items-center justify-center gap-5">
             <Image
+              className="w-[37.5rem]"
               src="https://drive.google.com/uc?export=view&id=1mzZ58SPWaVzOE2WsdK8RADLUT6hd178w"
               alt="Real Limpeza 6"
               width="607"
               height="462"
             />
             <Image
+              className="w-[38.5rem]"
               src="https://drive.google.com/uc?export=view&id=1jy4DqfZ0FO49L2DL7txGxUsdt6foZza-"
               alt="Real Limpeza 7"
               width="625"
               height="462"
             />
             <Image
+              className="w-[37.5rem]"
               src="https://drive.google.com/uc?export=view&id=1onDq4dHI-Xsv7w7KkTvfbjke9kb-oP6O"
               alt="Real Limpeza 8"
               width="607"
@@ -76,12 +84,14 @@ export default function RealLimpezaPage() {
           </div>
           <div className="flex flex-wrap items-center justify-center gap-5">
             <Image
+              className="w-[57.5rem]"
               src="https://drive.google.com/uc?export=view&id=1qdBsXe8AGKqir0dzM4MdCRgQjrz1Oev1"
               alt="Real Limpeza 9"
               width="930"
               height="695"
             />
             <Image
+              className="w-[57.5rem]"
               src="https://drive.google.com/uc?export=view&id=1uNxvzjWoXYUA-HZm7hIgEnjLcGEq42v7"
               alt="Real Limpeza 10"
               width="930"
@@ -89,6 +99,7 @@ export default function RealLimpezaPage() {
             />
           </div>
           <Image
+            className="w-[116rem]"
             src="https://drive.google.com/uc?export=view&id=1O51QlHoGBSVNY1dQFmFp_sJYd0E0G3jo"
             alt="Real Limpeza 11"
             width="1880"
@@ -96,12 +107,14 @@ export default function RealLimpezaPage() {
           />
           <div className="flex flex-wrap items-center justify-center gap-5">
             <Image
+              className="w-[57.5rem]"
               src="https://drive.google.com/uc?export=view&id=1qHiXAvpPlD2boeZVlTac0C7wQv-5Vj_S"
               alt="Real Limpeza 12"
               width="930"
               height="976"
             />
             <Image
+              className="w-[57.5rem]"
               src="https://drive.google.com/uc?export=view&id=1lJrTPct97WCc1resRKkrr1Ba7_WlvDLz"
               alt="Real Limpeza 13"
               width="930"
@@ -109,6 +122,7 @@ export default function RealLimpezaPage() {
             />
           </div>
           <Image
+            className="w-[116rem]"
             src="https://drive.google.com/uc?export=view&id=1yQOIzB9cAMYjIwVkXdEDWYdmTeaNy3uj"
             alt="Real Limpeza 14"
             width="1880"
@@ -116,12 +130,14 @@ export default function RealLimpezaPage() {
           />
           <div className="flex flex-wrap items-center justify-center gap-5">
             <Image
+              className="w-[51.5rem]"
               src="https://drive.google.com/uc?export=view&id=1w7Shi6092zgw7p2CgMldOGoBBUZdTK0n"
               alt="Real Limpeza 15"
               width="837"
               height="1018"
             />
             <Image
+              className="w-[63.5rem]"
               src="https://drive.google.com/uc?export=view&id=174UtCvnrNx0Iez5TL4H8RvpWLNltRHn6"
               alt="Real Limpeza 16"
               width="1023"
@@ -129,12 +145,14 @@ export default function RealLimpezaPage() {
             />
           </div>
           <Image
+            className="w-[116rem]"
             src="https://drive.google.com/uc?export=view&id=1U_SJQLVZzlBGW9zBmtWApxVy702-nPij"
             alt="Real Limpeza 17"
             width="1880"
             height="1228"
           />
           <Image
+            className="w-[116rem]"
             src="https://drive.google.com/uc?export=view&id=1OM-QIWd2YyNCbb5T_lFc1xhscwk_FNB-"
             alt="Real Limpeza 18"
             width="1880"
