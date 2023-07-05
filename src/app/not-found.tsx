602ebdadc80c3ef7633dc33ee750a45655802a3c
Murandido <murilo.candidomc97@gmail.com>
2023-07-05 14:59:14 -0300

    404 page done

---
+++ src/app/not-found.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Error 404 - Studio cauehenrique",
+};
+
+export default function NotFound() {
+  return (
+    <>
+      <title>Error 404 - Studio cauehenrique</title>
+      <main className="flex h-full min-w-[338.55px] items-center justify-center">
+        <div className="mx-5 flex flex-col items-center justify-center gap-12">
+          <h1 className="text-8xl uppercase text-primary-300 max-sm:text-4xl">
+            Error 404
+          </h1>
+          <p className="text-2xl max-sm:text-base">
+            The page you are looking for may have been moved, deleted, or
+            possibly never existed.{" "}
+            <Link className="text-primary-300 hover:underline" href="/">
+              Back to Home
+            </Link>
+          </p>
+        </div>
+      </main>
+    </>
+  );
+}
