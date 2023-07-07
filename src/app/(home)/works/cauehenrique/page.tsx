65b35597c4ca0f91d5e50bde56efa5aa77b30cb3
Murandido <murilo.candidomc97@gmail.com>
2023-07-07 14:23:57 -0300

    images added to cauehenrique page

--- src/app/(home)/works/cauehenrique/page.tsx
+++ src/app/(home)/works/cauehenrique/page.tsx
@@ -26,7 +26,102 @@ export default function RealLimpezaPage() {
         </div>
       </div>
       <div className="min-w-[338.55px] bg-white">
-        <div className="flex flex-wrap items-center justify-center gap-5 p-5"></div>
+        <div className="flex flex-wrap items-center justify-center gap-5 p-5">
+          <Image
+            src="https://i.imgur.com/mZapnpD.png"
+            alt="cauehenrique 1"
+            width="90"
+            height="90"
+          />
+          <Image
+            src="https://i.imgur.com/wpHZP8E.png"
+            alt="cauehenrique 2"
+            width="90"
+            height="90"
+          />
+          <Image
+            src="https://i.imgur.com/twUE7NM.png"
+            alt="cauehenrique 3"
+            width="90"
+            height="90"
+          />
+          <Image
+            src="https://i.imgur.com/ezhpW1O.png"
+            alt="cauehenrique 4"
+            width="90"
+            height="90"
+          />
+          <Image
+            src="https://i.imgur.com/f9A8RBz.png"
+            alt="cauehenrique 5"
+            width="90"
+            height="90"
+          />
+          <div>
+            <Image
+              src="https://i.imgur.com/7KZARcL.png"
+              alt="cauehenrique 6"
+              width="90"
+              height="90"
+            />
+            <Image
+              src="https://i.imgur.com/3FA4WZC.png"
+              alt="cauehenrique 7"
+              width="90"
+              height="90"
+            />
+          </div>
+          <Image
+            src="https://i.imgur.com/gDouBND.png"
+            alt="cauehenrique 8"
+            width="90"
+            height="90"
+          />
+          <Image
+            src="https://i.imgur.com/ENQovnz.png"
+            alt="cauehenrique 9"
+            width="90"
+            height="90"
+          />
+          <Image
+            src="https://i.imgur.com/GyTVYcv.png"
+            alt="cauehenrique 10"
+            width="90"
+            height="90"
+          />
+          <div>
+            <Image
+              src="https://i.imgur.com/ACWWEG4.png"
+              alt="cauehenrique 11"
+              width="90"
+              height="90"
+            />
+            <Image
+              src="https://i.imgur.com/WmOaQRQ.png"
+              alt="cauehenrique 12"
+              width="90"
+              height="90"
+            />
+          </div>
+          <Image
+            src="https://i.imgur.com/DVxA2g5.png"
+            alt="cauehenrique 13"
+            width="90"
+            height="90"
+          />
+          <Image
+            src="https://i.imgur.com/sP5wx7F.png"
+            alt="cauehenrique 14"
+            width="90"
+            height="90"
+          />
+          <Image
+            src="https://i.imgur.com/ZDaenNb.png"
+            alt="cauehenrique 15"
+            width="90"
+            height="90"
+          />
+        </div>
       </div>
     </main>
   );
