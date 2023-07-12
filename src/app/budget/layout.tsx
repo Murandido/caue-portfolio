b9561db64fa1e2bf3d19ec970b00382264e9f88c
Murandido <murilo.candidomc97@gmail.com>
2023-07-12 14:00:45 -0300

    new budget layout

--- src/app/budget/layout.tsx
+++ src/app/budget/layout.tsx
@@ -1,13 +1,11 @@
-import Footer from "@/components/Footer";
 import { ReactNode } from "react";
 import Header from "@/components/Header";
 
 export default function BudgetLayout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <div className="flex min-h-screen flex-col">
       <Header />
       {children}
-      <Footer page="budget" />
-    </>
+    </div>
   );
 }
