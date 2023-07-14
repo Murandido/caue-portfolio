d9f75c21f11b1d124de01b63cd4fc9551903589f
Murandido <murilo.candidomc97@gmail.com>
2023-07-14 16:43:51 -0300

    hotfix: updated InfoTopic component and added an link to a topic

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import InfoTopic from "./InfoTopic";
 
 export default function Info() {
@@ -11,11 +12,44 @@ export default function Info() {
           </h1>
         </div>
         <div>
-          <InfoTopic text="We are a design studio specializing in creating strategic visual identities for unique companies, as well as website development. With a passion for art and a deep understanding of the power of design, we are here to help your brand stand out in a competitive market." />
-          <InfoTopic text="With extensive experience., we combine creativity and strategy to develop memorable visual identities. We believe that every company has a unique story to tell, and our mission is to translate that story into an authentic visual identity." />
-          <InfoTopic text="Our client-centric approach allows us to understand your needs and business objectives, ensuring that each project is carefully tailored to reflect the personality of your brand. We utilize a combination of strategic colors, carefully selected typography, and intelligent design to create a visual identity that effectively communicates your message." />
-          <InfoTopic text="At Studio Cauehenrique, we value long-lasting relationships and work closely with our clients from conception to implementation. Our goal is not only to deliver stunning design but also to provide strategic design solutions that propel your business and set you apart in the market." />
-          <InfoTopic text="Explore our portfolio to see some of the projects we have completed and be inspired by our unique approach. We are eager to collaborate with you, create your visual identity, and develop a website that leaves a lasting impression." />
+          <InfoTopic>
+            We are a design studio specializing in creating strategic visual
+            identities for unique companies, as well as website development.
+            With a passion for art and a deep understanding of the power of
+            design, we are here to help your brand stand out in a competitive
+            market.
+          </InfoTopic>
+          <InfoTopic>
+            With extensive experience., we combine creativity and strategy to
+            develop memorable visual identities. We believe that every company
+            has a unique story to tell, and our mission is to translate that
+            story into an authentic visual identity.
+          </InfoTopic>
+          <InfoTopic>
+            Our client-centric approach allows us to understand your needs and
+            business objectives, ensuring that each project is carefully
+            tailored to reflect the personality of your brand. We utilize a
+            combination of strategic colors, carefully selected typography, and
+            intelligent design to create a visual identity that effectively
+            communicates your message.
+          </InfoTopic>
+          <InfoTopic>
+            At Studio Cauehenrique, we value long-lasting relationships and work
+            closely with our clients from conception to implementation. Our goal
+            is not only to deliver stunning design but also to provide strategic
+            design solutions that propel your business and set you apart in the
+            market.
+          </InfoTopic>
+          <InfoTopic>
+            Explore our{" "}
+            <Link className="text-primary-300 hover:underline" href="/">
+              portfolio
+            </Link>{" "}
+            to see some of the projects we have completed and be inspired by our
+            unique approach. We are eager to collaborate with you, create your
+            visual identity, and develop a website that leaves a lasting
+            impression.
+          </InfoTopic>
         </div>
       </div>
     </div>
--- src/components/InfoTopic.tsx
+++ src/components/InfoTopic.tsx
@@ -1,8 +1,10 @@
-export default function InfoTopic({ text }: { text: string }) {
+import { ReactNode } from "react";
+
+export default function InfoTopic({ children }: { children: ReactNode }) {
   return (
     <div className="flex justify-center border-b border-[#D9D9D9] text-2xl max-sm:text-xs">
       <p className="mx-[8.75rem] my-24 max-w-[79.5rem] max-lg:mx-5 max-sm:my-12 max-xs:mx-0">
-        {text}
+        {children}
       </p>
     </div>
   );
