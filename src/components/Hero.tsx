66bdd2e74f59f1b9410a6019d80dc1e3136393f3
Murandido <murilo.candidomc97@gmail.com>
2023-07-14 13:15:30 -0300

    renamed HeroIndex component to Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-export default function HeroIndex() {
+export default function Hero() {
   return (
     <div className="flex min-w-[338.55px]">
       <div className="mx-40 my-60 w-[80rem] text-[4rem] max-lg:mx-10 max-sm:text-xl max-xs:mx-5">
