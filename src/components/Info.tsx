3da994cae068a6a7f70db9c5a1ed34ef6c333b8e
Murandido <murilo.candidomc97@gmail.com>
2023-07-14 16:19:58 -0300

    hotfix: some margin sizes

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -2,10 +2,10 @@ import InfoTopic from "./InfoTopic";
 
 export default function Info() {
   return (
-    <div className="m-5 flex justify-center font-robotoSlab">
+    <div className="m-5 mb-0 flex justify-center font-robotoSlab">
       <div className="flex max-w-[100rem] flex-col">
         <div className="flex justify-center border-b border-[#D9D9D9] font-righteous text-4xl leading-normal max-sm:text-base">
-          <h1 className="my-24 max-w-[46rem] max-sm:my-12">
+          <h1 className="mx-[9.25rem] my-24 max-w-[59.5rem] text-center max-lg:mx-7 max-sm:my-12 max-xs:mx-2">
             Design that communicates with the right audience, stands out, and
             attracts.
           </h1>
