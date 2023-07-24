8de79ac36d49f3435619a88c1af9bb2e9819ecac
Murandido <murilo.candidomc97@gmail.com>
2023-07-24 15:45:27 -0300

    Hero component in about page internationalizated

--- src/app/[lang]/(home)/about/page.tsx
+++ src/app/[lang]/(home)/about/page.tsx
@@ -1,20 +1,32 @@
 import AboutCaue from "@/components/AboutCaue";
 import Hero from "@/components/Hero";
 import Info from "@/components/Info";
+import { getDictionary } from "@/get-dictionary";
+import { Locale } from "@/i18n-config";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "About - Studio cauehenrique",
+  title: "About | Studio Cauê Henrique",
 };
 
-export default function AboutPage() {
+export default async function AboutPage({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const dictionary = await getDictionary(lang);
+
   return (
     <main className="min-w-[338.55px]">
       <Hero>
-        <span className="text-primary-300">It&apos;s about</span> creating
-        creative solutions for companies that need to{" "}
-        <span className="text-primary-300">differentiate</span> themselves in
-        the market scenario.
+        <span className="text-primary-300">
+          {dictionary.components.Hero.about.highlight1}
+        </span>
+        {dictionary.components.Hero.about.text1}
+        <span className="text-primary-300">
+          {dictionary.components.Hero.about.highlight2}
+        </span>
+        {dictionary.components.Hero.about.text2}
       </Hero>
       <AboutCaue />
       <Info />
