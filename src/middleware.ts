071fe7db65dc9ada8696b34a9a85c0a7c4bea5fa
Murandido <murilo.candidomc97@gmail.com>
2023-07-24 13:54:39 -0300

    new middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,44 @@
 import { NextRequest, NextResponse } from "next/server";
-import { match } from "@formatjs/intl-localematcher";
+import { match as matchLocale } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
+import { i18n } from "./i18n-config";
 
-const locales = ["en", "pt"];
-const defaultLocale = "en";
+function getLocale(request: NextRequest): string | undefined {
+  const negotiatorHeaders: Record<string, string> = {};
+  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
 
-function getLocale(request: NextRequest) {
-  const headers = {
-    "accept-language": request.headers.get("accept-language") ?? "",
-  };
-  const languages = new Negotiator({ headers }).languages();
+  // @ts-ignore locales are readonly
+  const locales: string[] = i18n.locales;
 
-  return match(languages, locales, defaultLocale);
+  const languages = new Negotiator({ headers: negotiatorHeaders }).languages(
+    locales,
+  );
+
+  const locale = matchLocale(languages, locales, i18n.defaultLocale);
+
+  return locale;
 }
 
 export function middleware(request: NextRequest) {
-  const preferredLocale = getLocale(request);
+  const pathname = request.nextUrl.pathname;
 
-  return NextResponse.redirect(new URL(`/${preferredLocale}/`, request.url));
+  const pathnameIsMissingLocale = i18n.locales.every(
+    (locale) =>
+      !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
+  );
+
+  if (pathnameIsMissingLocale) {
+    const locale = getLocale(request);
+
+    return NextResponse.redirect(
+      new URL(
+        `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
+        request.url,
+      ),
+    );
+  }
 }
 
 export const config = {
-  matcher: "/",
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
