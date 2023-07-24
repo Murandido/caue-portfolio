fe66886c52f9a88f4a0722779a0609d56a0e2072
Murandido <murilo.candidomc97@gmail.com>
2023-07-24 16:20:41 -0300

    budget page internationalizated

--- src/app/[lang]/budget/page.tsx
+++ src/app/[lang]/budget/page.tsx
@@ -1,23 +1,28 @@
+import { getDictionary } from "@/get-dictionary";
+import { Locale } from "@/i18n-config";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Budget - Studio cauehenrique",
 };
 
-export default function Budget() {
+export default async function Budget({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const dictionary = await getDictionary(lang);
+
   return (
     <main className="flex min-w-[338.55px] flex-grow">
       <section className="mx-40 flex items-center font-robotoSlab text-2xl leading-normal max-lg:mx-10 max-sm:text-sm max-xs:mx-5">
         <div className="slideUp flex max-w-[40rem] flex-col gap-6 max-sm:gap-2">
           <div className="flex">
             <h1 className="font-righteous text-[4rem] max-sm:text-4xl">
-              Contact-us
+              {dictionary.budget.title}
             </h1>
           </div>
-          <p>
-            Are you in need of a comprehensive visual identity quote? Send an
-            e-mail to:
-          </p>
+          <p>{dictionary.budget.text}</p>
           <div className="flex">
             <a
               className="text-primary-300 hover:underline"
