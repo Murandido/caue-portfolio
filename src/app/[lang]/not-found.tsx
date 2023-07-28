39f7da9052a79eab4020918b74bfa86bf7565baa
Murandido <murilo.candidomc97@gmail.com>
2023-07-28 14:44:27 -0300

    404 page fixed but with gambiarra

--- src/app/[lang]/not-found.tsx
+++ src/app/[lang]/not-found.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <>
-      <title>Error 404 - Studio cauehenrique</title>
+      <title>Error 404 | Studio Cauê Henrique</title>
       <main className="flex h-full min-w-[338.55px] items-center justify-center">
         <div className="mx-5 flex flex-col items-center justify-center gap-12">
           <h1 className="text-8xl uppercase text-primary-300 max-sm:text-4xl">
