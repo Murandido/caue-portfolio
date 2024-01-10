d537da8efad01920eb90bdff7e9c99d9bc7858a3
Murandido <murilo.candidomc97@gmail.com>
2024-01-10 16:14:02 -0300

    metadata added for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import Carousel from "@/components/Carousel";
 import Hero from "@/components/Hero";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Cauê Henrique | Brand design studio",
+  description:
+    "If your design doesn't speak to the right audience, who is it telling its story to?",
+};
 
 export default function Home() {
   const slides = [
