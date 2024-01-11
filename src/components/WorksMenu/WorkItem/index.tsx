542fe50c6b12386d43c90d9d176731ac797dd4ae
Murandido <murilo.candidomc97@gmail.com>
2024-01-11 14:58:09 -0300

    hover of work items done

--- src/components/WorksMenu/WorkItem/index.tsx
+++ src/components/WorksMenu/WorkItem/index.tsx
@@ -8,16 +8,21 @@ interface WorkItemProps {
 
 export default function WorkItem({ name, image, link }: WorkItemProps) {
   return (
-    <a href={link}>
+    <a href={link} className="relative flex">
       <div>
         <Image
+          className="rounded-xl"
           src={image}
           alt={`${name.toLowerCase()} page`}
           width={920}
           height={800}
         />
       </div>
-      <div className="hidden">{name.toUpperCase()}</div>
+      <div className="absolute h-full w-full rounded-xl bg-[rgba(32,32,32,0.5)] opacity-0 transition duration-500 ease-out hover:opacity-100">
+        <div className="absolute bottom-0 mx-10 my-8 text-[2.5rem] font-bold leading-[150%] text-white max-lg:text-3xl max-md:mx-6 max-md:my-4 max-md:text-xl max-xs:text-base">
+          {name.toUpperCase()}
+        </div>
+      </div>
     </a>
   );
 }
