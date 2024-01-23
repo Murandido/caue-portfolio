622bd0fcfb6121e87d8023784c45cde1b58ef80c
Murandido <murilo.candidomc97@gmail.com>
2024-01-23 20:51:06 -0300

    articles hero done

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,3 +1,16 @@
+import ArticlesHero from "@/components/ArticlesHero";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Articles | Cauê Henrique studio",
+  description:
+    "There is no more effective way to showcase expertise than through articles. Come and discover our visual identity studies.",
+};
+
 export default function Articles() {
-  return <div>Articles</div>;
+  return (
+    <div>
+      <ArticlesHero />
+    </div>
+  );
 }
