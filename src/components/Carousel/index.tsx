6c6eabe1d6f9941aa1173e678b3643a93eb5756c
Murandido <murilo.candidomc97@gmail.com>
2024-09-05 18:29:37 -0300

    fix custom cursor in carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { MouseEvent, useEffect, useState } from "react";
+import { motion, Variants } from "framer-motion";
 
 interface CarouselProps {
   slides: string[];
@@ -7,7 +8,18 @@ interface CarouselProps {
 
 export default function Carousel({ slides }: CarouselProps) {
   const [currentSlide, setCurrentSlide] = useState<number>(0);
+  const [cursorURL, setCursorURL] = useState<
+    "/leftArrowIcon.svg" | "/rightArrowIcon.svg"
+  >("/leftArrowIcon.svg");
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [mouseAnimation, setMouseAnimation] = useState<"default" | "moving">(
+    "default",
+  );
 
+  // carousel logic
   useEffect(() => {
     const interval = setInterval(() => {
       if (currentSlide === slides.length - 1) setCurrentSlide(0);
@@ -37,6 +49,55 @@ export default function Carousel({ slides }: CarouselProps) {
     }
   };
 
+  // custom cursor logic
+  const mouseMove = (e: globalThis.MouseEvent) => {
+    setMousePosition({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+  // useEffect(() => {
+
+  //
+
+  //   return () => {
+  //
+  //   };
+  // }, []);
+
+  const variants: Variants = {
+    default: {
+      opacity: 0,
+      transition: { duration: 0 },
+    },
+    moving: {
+      x: mousePosition.x - 40,
+      y: mousePosition.y - 40,
+      transition: { duration: 0 },
+    },
+  };
+
+  const handleMouseEnterLeftInvisibleArrow = () => {
+    window.addEventListener("mousemove", mouseMove);
+    setCursorURL("/leftArrowIcon.svg");
+    setMouseAnimation("moving");
+  };
+
+  const handleMouseLeaveLeftInvisibleArrow = () => {
+    setMouseAnimation("default");
+    window.removeEventListener("mousemove", mouseMove);
+  };
+
+  const handleMouseEnterRightInvisibleArrow = () => {
+    window.addEventListener("mousemove", mouseMove);
+    setCursorURL("/rightArrowIcon.svg");
+    setMouseAnimation("moving");
+  };
+
+  const handleMouseLeaveRightInvisibleArrow = () => {
+    setMouseAnimation("default");
+    window.removeEventListener("mousemove", mouseMove);
+  };
   return (
     <div className="w-full max-xl:hidden">
       <div className="relative overflow-hidden">
@@ -64,12 +125,47 @@ export default function Carousel({ slides }: CarouselProps) {
             Design that communicates attracts and creates bonds.
           </div>
         </div>
-        <div className="absolute bottom-8 left-1/2 flex -translate-x-1/2 flex-col items-center gap-11">
+
+        {/* cursor */}
+        <motion.div
+          className="fixed left-0 top-0"
+          variants={variants}
+          animate={mouseAnimation}
+        >
+          <Image
+            className="h-20 w-20"
+            alt="mouse cursor"
+            src={cursorURL}
+            width={32}
+            height={32}
+          />
+        </motion.div>
+
+        {/* invisible arrows */}
+        <div
+          onMouseDown={handleClickLeft}
+          onMouseEnter={handleMouseEnterLeftInvisibleArrow}
+          onMouseLeave={handleMouseLeaveLeftInvisibleArrow}
+          className="absolute left-0 top-0 h-full w-1/2 cursor-none"
+        />
+        <div
+          onMouseDown={handleClickRight}
+          onMouseEnter={handleMouseEnterRightInvisibleArrow}
+          onMouseLeave={handleMouseLeaveRightInvisibleArrow}
+          className="absolute right-0 top-0 h-full w-1/2 cursor-none"
+        />
+
+        {/* button */}
+        <div className="absolute bottom-[4.75rem] left-1/2 flex -translate-x-1/2 flex-col items-center gap-11">
           <div>
             <button className="rounded-full border border-black px-[3.25rem] py-3 font-gelica text-xl transition hover:bg-black hover:text-white">
               Discover our method
             </button>
           </div>
+        </div>
+
+        {/* carousel  bullets */}
+        <div className="absolute bottom-8 left-1/2 flex -translate-x-1/2 flex-col items-center gap-11">
           <div className="flex gap-2 transition">
             {slides.map((_, index) => {
               return (
@@ -83,16 +179,6 @@ export default function Carousel({ slides }: CarouselProps) {
             })}
           </div>
         </div>
-
-        {/* arrows */}
-        <div
-          onMouseDown={handleClickLeft}
-          className="absolute left-0 top-0 h-full w-1/2 cursor-[url('/leftArrowIcon.svg'),pointer]"
-        />
-        <div
-          onMouseDown={handleClickRight}
-          className="absolute right-0 top-0 h-full w-1/2 cursor-[url('/rightArrowIcon.svg'),pointer] "
-        />
       </div>
     </div>
   );
